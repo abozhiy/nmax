281a9895887c32f6fd61147a09934ce7c2baa7ab
Anton Bozhiy <anton.bozhiy@lakehouse.ru>
2017-10-22 18:01:44 +0300

    added spec for Nmax result method

--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe Nmax do
     expect(Nmax::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'result' do
+
+    it "should return integer if arg is not nil" do
+      expect(Nmax.result(2, File.open("./text.txt"))).to eq [234313413243, 3452352345]
+    end
+
+    it "shouldn't return integer if arg is nil" do
+      expect(Nmax.result(nil, File.open("./text.txt"))).to eq []
+    end
   end
 end
